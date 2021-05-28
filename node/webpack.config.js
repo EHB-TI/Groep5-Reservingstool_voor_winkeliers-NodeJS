84d48245377c61d8774b166eeeb6e03f156458c8
Faille Arnaud <arnaudar.faille@gmail.com>
2021-05-28 16:14:31 +0200

    Basic selects from database

--- node/webpack.config.js
+++ node/webpack.config.js
@@ -1,14 +1,23 @@
 const path = require('path');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 module.exports = {
+  resolve : {
+    fallback: {
+      util: require.resolve("util/")
+    }
+  },
   entry: {
-    db: './src/db.js',
-    store: './src/store.js'
+      db: "./src/db.js",
+      store: "./src/store.js"
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, '../js'),
+    path: path.resolve(__dirname, '../public/js'),
   },
-  mode: "development",
-  watch: true
+  mode: "development"/*,
+  watch: true*/,
+  plugins: [
+    new NodePolyfillPlugin()
+]
 };
