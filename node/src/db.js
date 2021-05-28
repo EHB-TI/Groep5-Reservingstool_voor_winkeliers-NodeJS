d509d669501b15bb79803686f8e6e67cad649c1c
Faille Arnaud <arnaudar.faille@gmail.com>
2021-05-28 11:57:47 +0200

    First commit

---
+++ node/src/db.js
@@ -0,0 +1,15 @@
+/**
+ * Author: Arnaud Faille
+ * Voorbeelden van https://www.youtube.com/watch?v=5xFhSNYegmc&list=PLGsnrfn8XzXiXHBut72-hqSkXKjxY6Nin&index=7&ab_channel=MikeDeryckeMikeDerycke
+ */
+
+console.log("test");
+
+/*const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: "dt5.ehb.be",
+    user: "2021PROGPROJGR5",
+    password: "8uGuEtMV",
+    database: "2021PROGPROJGR5"
+});*/
