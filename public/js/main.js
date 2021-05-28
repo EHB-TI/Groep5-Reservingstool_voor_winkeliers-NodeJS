8b1ef1d8e39525a6f5693892d535ef8ba182647e
RyanDV38 <ryan.de.vogel@ehb.be>
2021-05-28 18:26:32 +0200

    added API to main.js and changed filenames, cleaned up..

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-let star = document.querySelectorAll('input');
+let star = document.querySelectorAll('input[type="radio"]');
 
 for (let i = 0; i < star.length; i++) {
 	star[i].addEventListener('click', function() {
@@ -13,6 +13,17 @@ document.getElementsByClassName("button-review")[0].addEventListener("click", fu
 	window.location.replace("../reserveer/reserveer.html")//Is tijdelijk moet naar andere pagina gelinkt worden
   });
 
+function sendDate(){
+	let btn = document.querySelectorAll(".reserveer-btn")[0];
+	console.log(btn.value);
+}
+
 function register(){
 	alert("lol");
+}
+
+function fetchData(){
+  fetch('http://localhost:3000/api/insert/reservation?store_id=2&customer_id=1&date=2021-05-25%2017:14:06')
+  .then(response => response.json())
+  .then(data => console.log(data));
 }
