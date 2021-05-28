7e8ac3c31e7f5093c5a56fd2c8ab3eea7e64bd0e
Faille Arnaud <arnaudar.faille@gmail.com>
2021-05-28 16:19:04 +0200

    Update

--- node/webpack.config.js
+++ node/webpack.config.js
@@ -1,12 +1,6 @@
 const path = require('path');
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 module.exports = {
-  resolve : {
-    fallback: {
-      util: require.resolve("util/")
-    }
-  },
   entry: {
       db: "./src/db.js",
       store: "./src/store.js"
@@ -16,8 +10,5 @@ module.exports = {
     path: path.resolve(__dirname, '../public/js'),
   },
   mode: "development"/*,
-  watch: true*/,
-  plugins: [
-    new NodePolyfillPlugin()
-]
+  watch: true*/
 };
