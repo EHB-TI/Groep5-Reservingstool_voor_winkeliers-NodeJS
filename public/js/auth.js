514b38704741d1d3bfaeeb8d222eca2695fc9fd2
Faille Arnaud <arnaudar.faille@gmail.com>
2021-05-31 15:50:39 +0200

    Update API

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,5 +1,10 @@
+/**
+ * Author: Arnaud Faille
+ * Code inspired from https://stackoverflow.com/questions/29775797/fetch-post-json-data
+ * @returns User to json value if logged in
+ */
 async function getUser() {
-    const rawResponse = await fetch('../api/auth/me', {
+    const rawResponse = await fetch('http://localhost:3000/api/auth/me', {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
             'x-access-token': encodeURIComponent(localStorage.getItem('token'))
--- public/js/tickets.js
+++ public/js/tickets.js
@@ -1,32 +1,40 @@
+/**
+ * Author: Arnaud Faille
+ */
 window.onload = () => {
-    getData("../api/get/tickets?customer_id=1").then(value => {
-        let ticketsBody = document.getElementById("tickets");
-
-        for(row of value){
-            let ticketTr = document.createElement("tr");
-            let ticketIdTh = document.createElement("th");
-            let ticketTitleTd = document.createElement("td");
-            let ticketTitleA = document.createElement("a");
-            let ticketStatusTd = document.createElement("td");
-
-            ticketIdTh.innerText = row.id;
-            ticketTitleA.innerText = row.title;
-            ticketTitleA.href = `view?id=${row.id}`;
-            ticketTitleA.classList.add("text-dark");
-            ticketTitleA.classList.add("text-decoration-none");
-            ticketTitleA.classList.add("d-block");
-            ticketTitleA.classList.add("w-100");
-            ticketStatusTd.innerText = (row.status == 1? "Open": "Closed");
-            ticketStatusTd.classList.add(row.status == 1? "table-success": "table-danger");
-
-            ticketTr.appendChild(ticketIdTh);
-            ticketTitleTd.appendChild(ticketTitleA);
-            ticketTr.appendChild(ticketTitleTd);
-            ticketTr.appendChild(ticketStatusTd);
-            ticketsBody.appendChild(ticketTr);
+    getUser().then(user => {
+        if(user.auth == false){
+            window.location.href = "../login";
         }
-    }, error => {
-        console.log(error);
+        getData(`../api/get/tickets?customer_id=${user.id}`).then(value => {
+            let ticketsBody = document.getElementById("tickets");
+    
+            for(row of value){
+                let ticketTr = document.createElement("tr");
+                let ticketIdTh = document.createElement("th");
+                let ticketTitleTd = document.createElement("td");
+                let ticketTitleA = document.createElement("a");
+                let ticketStatusTd = document.createElement("td");
+    
+                ticketIdTh.innerText = row.id;
+                ticketTitleA.innerText = row.title;
+                ticketTitleA.href = `view?id=${row.id}`;
+                ticketTitleA.classList.add("text-dark");
+                ticketTitleA.classList.add("text-decoration-none");
+                ticketTitleA.classList.add("d-block");
+                ticketTitleA.classList.add("w-100");
+                ticketStatusTd.innerText = (row.status == 1? "Open": "Closed");
+                ticketStatusTd.classList.add(row.status == 1? "table-success": "table-danger");
+    
+                ticketTr.appendChild(ticketIdTh);
+                ticketTitleTd.appendChild(ticketTitleA);
+                ticketTr.appendChild(ticketTitleTd);
+                ticketTr.appendChild(ticketStatusTd);
+                ticketsBody.appendChild(ticketTr);
+            }
+        }, error => {
+            console.log(error);
+        });
     });
 
     async function getData(url){
