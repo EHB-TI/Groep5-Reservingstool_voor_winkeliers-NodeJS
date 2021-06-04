1a3080a1654aff94c37cb53cee28ab8262fd7212
RyanDV38 <80149519+RyanDV38@users.noreply.github.com>
2021-06-04 19:06:35 +0200

    Added documentation

--- public/js/registreer.js
+++ public/js/registreer.js
@@ -4,6 +4,7 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const storeId = urlParams.get('store');
 
+//Check if user chose a store
 getData(`http://localhost:3000/api/get/stores?id=${storeId}`).then(data=>{
   if(Object.keys(data).length > 0){
     console.log("Je zit op de goeie pagina");
@@ -12,19 +13,21 @@ getData(`http://localhost:3000/api/get/stores?id=${storeId}`).then(data=>{
     console.log("Je zit op de foute pagina");
   }
 });
-
+//Get the data
 async function getData(url){
   let data = await fetch(url);
     return await data.json();
 };
 
 async function Reserveer(){
+  //Check if user is logged in
   let user = await getUser();
   if(user.auth == false){
     alert("Je moet eerst ingelogd zijn!")
     window.location.href = "../login/";
   }
   else {
+    //Get the values off the input field and check if they are empty
     let date = document.getElementById("date-picker").value;
   let getHour = document.getElementById("uren").value;
   let getMinute = document.getElementById("minuten").value;
@@ -32,6 +35,7 @@ async function Reserveer(){
     alert("Je moet alles invullen voor je kan registreren!");
   }
   else {
+    //Store the reservation in the database with the values off the variabeles
     getData(`http://localhost:3000/api/insert/reservations?store_id=${storeId}&customer_id=${user.id}&date=${date}%20${getHour}:${getMinute}:00`).then(data=>{
     console.log(data);
     alert(`Je plaats is gereserveerd voor: ${date} ${getHour}:${getMinute}.`)
